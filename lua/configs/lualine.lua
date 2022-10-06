0c1d0cc565918134b8048b29186ab7644cbf96bd
CoderLadFahim <fahimalemroz@gmail.com>
2022-10-06 06:15:14 +0600

    Prettier is now fully functional.

---
+++ lua/configs/lualine.lua
@@ -0,0 +1,52 @@
+local themes = {
+  Sunday = "molokai",
+  Monday = "onedark",
+  Tuesday = "everforest", -- using a different theme as an angr theme doesn't exist for lualine
+  Wednesday = "nord",
+  Thursday = "tokyonight",
+  Friday = "gruvbox",
+  Saturday = "ayu",
+}
+
+local current_day = os.date("%A");
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = themes[current_day],
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
